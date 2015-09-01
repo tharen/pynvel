7fd240a4b03c8227d3b7db26d04a07591b6d3a8b
tod haren <tod.haren@gmail.com>
2015-09-01 12:59:07 -0700

    Update CMakeLists.txt     - Update build environment to use MinGW64 from MSYS2

--- _msys2-mingw64.bat
+++ _msys2-mingw64.bat
@@ -1,41 +1,40 @@
 @echo off
+:: Build envirnonment for NVEL/PyNVEL
 
-::cd /d %~dp0
+set srcroot=C:\workspace\src\nvel
 
 set USERPROFILE=c:\users\tharen
 set HOMEDRIVE=c:
 set HOMEPATH=\users\tharen
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
-cd /d %HOME%
-
 set progsroot=C:\progs
-set srcroot=C:\workspace\src\nvel
-
-set PATH=%windir%\system32\;%PATH%
-::set PATH=%progsroot%\TDM-GCC-64\bin;%PATH%
 set PATH=%PATH%;%progsroot%\cmake\bin
 set PATH=%PATH%;%progsroot%\TortoiseHg
 set PATH=%PATH%;%progsroot%\TortoiseSVN\bin
+set PATH=%PATH%;%progsroot%\pythonxy\WinMerge-2.12.4.2
+set PATH=%PATH%;%progsroot%\notepad++
 set PATH=%PATH%;C:\progs\graphviz\bin;C:\progs\doxygen\bin
-:: MSYSGit includes sh.exe which conflicts with mingw32-make
-::set PATH=%PATH%;%progsroot%\Git\bin
-set PATH=%PATH%;%progsroot%\notepad++;%progsroot%\pythonxy\WinMerge-2.12.4.2
-set PATH=C:\Python27\DLLs;%PATH%
-set PATH=c:\python27;c:\python27\scripts;%PATH%
+set PATH=%windir%\system32\;%PATH%
 
+set PATH=c:\python27;C:\Python27\DLLs;c:\python27\scripts;%PATH%
+
+:: TODO: Need some fancy way of updating the build type
 set PYTHONPATH=%srcroot%\debug
 set PATH=%srcroot%\debug;%PATH%
 
 set PROMPT=$p$_$+$g
 
+cd /d %srcroot%
+
 :: Set PATH values for GCC
-call c:\progs\TDM-GCC-64\mingwvars.bat
+::call c:\progs\TDM-GCC-64\mingwvars.bat
+
 set MSYSTEM=MINGW32
 
 set CHERE_INVOKING=1
 start c:\progs\console2\console.exe ^
 		-d %srcroot% ^
-		-w "Open-FVS (TDM-GCC-64)" ^
+		-w "Open-FVS (MSY2+MinGW-w64 %MSYSTEM%)" ^
 		-r "cmd /C C:\progs\msys64\usr\bin\bash.exe --login -i"
 ::		-t "MSYS2 Bash" -r "%*"
