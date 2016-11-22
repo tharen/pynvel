6ea53e02e9c2c09447a80563d1e7bd76bbe7f662
Tod Haren <tod.haren@gmail.com>
2016-11-22 02:09:56 -0800

    Test with MinGW w64 "6.2.0-release-win32-seh-rt_v5-rev1"

--- cmd_mingw-w64.bat
+++ cmd_mingw-w64.bat
@@ -3,7 +3,8 @@ setlocal
 
 set proj_root=%~dp0
 
-set PATH=C:\progs\mingw-w64\x86_64-5.3.0-win32-seh-rt_v4-rev0\mingw64\bin
+set PATH=C:\progs\mingw-w64\x86_64-6.2.0-release-win32-seh-rt_v5-rev1\mingw64\bin
+REM set PATH=C:\progs\mingw-w64\x86_64-5.3.0-win32-seh-rt_v4-rev0\mingw64\bin
 REM set PATH=C:\progs\mingw-w64\x86_64-5.3.0-posix-seh-rt_v4-rev0\mingw64\bin
 REM set PATH=C:\progs\mingw-w64\x86_64-5.1.0-win32-seh-rt_v4-rev0\mingw64\bin
 
